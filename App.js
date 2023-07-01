523772c7583335b61ea7bf4009845a8610498ede
gurudev17 <gurudev.m@turtlemint.com>
2023-07-01 12:33:32 +0530

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Your Vehicle need Inspection</Text>
+      
+      <Button
+      onPress={() => this._handlePress()}
+      title="Start Inspection"></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +21,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
